84e3ec294c94de3a703336f40321c8d432979c20
Paul Razvan Berg <hello@paulrberg.com>
2020-07-07 15:59:39 +0300

    fix: add pre and post hooks for coverage script

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,15 @@
+const shell = require("shelljs");
+
 module.exports = {
   istanbulReporter: ["html"],
   mocha: {
     delay: true,
+  },
+  onCompileComplete: async function (_config) {
+    await run("typechain");
+  },
+  onIstanbulComplete: async function (_config) {
+    shell.rm("-rf", "./typechain");
   },
   skipFiles: ["mocks", "test"],
 };
