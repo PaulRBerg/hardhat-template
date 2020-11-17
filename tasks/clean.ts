87a40601230f94553973fa4c38637982746b650d
Tom French <tom@tomfren.ch>
2020-11-17 21:49:48 +0000

    fix: re-enable cleaning of coverage files

--- tasks/clean.ts
+++ tasks/clean.ts
@@ -3,8 +3,8 @@ import { TASK_CLEAN } from "hardhat/builtin-tasks/task-names";
 import { task } from "hardhat/config";
 
 task(TASK_CLEAN, "Overrides the standard clean task", async function (_taskArgs, { config }, runSuper) {
-  // await fsExtra.remove(config.paths.coverage);
-  // await fsExtra.remove(config.paths.coverageJson);
+  await fsExtra.remove("./coverage");
+  await fsExtra.remove("./coverage.json");
   if (config.typechain?.outDir) {
     await fsExtra.remove(config.typechain.outDir);
   }
