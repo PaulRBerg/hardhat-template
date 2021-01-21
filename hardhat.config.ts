8307adaeb750f3e786b7cf5263e5008a07f6f0cf
Tom French <tom@tomfren.ch>
2021-01-21 16:45:01 +0000

    feat: update to solidity 0.8.0

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -72,7 +72,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.7.4",
+    version: "0.8.0",
     settings: {
       // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
