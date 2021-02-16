1ad3da68694ddb5877b3be52796718ed4b31672b
Paul Razvan Berg <hello@paulrberg.com>
2021-02-16 15:13:15 +0200

    test: add 'loadFixture' to Mocha context augmentation

--- types/augmentations.d.ts
+++ types/augmentations.d.ts
@@ -1,9 +1,13 @@
+// eslint-disable @typescript-eslint/no-explicit-any
+import { Fixture } from "ethereum-waffle";
+
 import { Signers } from "./";
 import { Greeter } from "../typechain";
 
 declare module "mocha" {
   export interface Context {
     greeter: Greeter;
+    loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
 }
