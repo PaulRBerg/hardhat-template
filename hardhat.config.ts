98fbf71e8766b3a1568c0be4815cb971a73988f1
Paul Razvan Berg <hello@paulrberg.com>
2021-02-17 14:31:10 +0200

    feat: upgrade to solc@0.8.1
    build: upgrade hardhat and its plugins to the latest version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -71,7 +71,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.7.4",
+    version: "0.8.1",
     settings: {
       // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
