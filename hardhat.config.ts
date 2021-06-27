8156684d15aff71e0cfc57b649ee38936dac2162
Paul Razvan Berg <hello@paulrberg.com>
2021-06-27 13:24:29 +0300

    build: upgrade deps
    feat: bump compiler version to v0.8.6

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -76,7 +76,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.5",
+    version: "0.8.6",
     settings: {
       metadata: {
         // Not including the metadata hash
