07f9f2225a5e0f03d29ad2172e7f1378aa8f0e18
Paul Razvan Berg <hello@paulrberg.com>
2021-08-08 15:25:09 +0300

    build: remove stale files from include field in tsconfig.json
    test: fix greeting equality check

--- test/greeter/Greeter.behavior.ts
+++ test/greeter/Greeter.behavior.ts
@@ -4,7 +4,7 @@ export function shouldBehaveLikeGreeter(): void {
   it("should return the new greeting once it's changed", async function () {
     expect(await this.greeter.connect(this.signers.admin).greet()).to.equal("Hello, world!");
 
-    await this.greeter.setGreeting("Hola, mundo!");
+    await this.greeter.setGreeting("Bonjour, le monde!");
     expect(await this.greeter.connect(this.signers.admin).greet()).to.equal("Bonjour, le monde!");
   });
 }
