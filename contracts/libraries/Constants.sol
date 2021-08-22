326f76a2f870395099f4326326db35327c5d804b
Marius Baisan <marius.baisan@cst-us.com>
2021-08-22 12:49:05 +0300

    Added a constants library plus a util contract that for now has a uint to str convertor

---
+++ contracts/libraries/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+library Constants { 
+
+    uint256 internal constant UINT256_MAX = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+    uint256 internal constant DECIMALS_18 = 1000000000000000000;
+
+}
---
+++ contracts/utils/Util.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+contract Util {
+  function uint2str(uint256 _i) public pure returns (string memory _uintAsString) {
+    if (_i == 0) {
+      return "0";
+    }
+    uint256 j = _i;
+    uint256 len;
+    while (j != 0) {
+      len++;
+      j /= 10;
+    }
+    bytes memory bstr = new bytes(len);
+    uint256 k = len;
+    while (_i != 0) {
+      k = k - 1;
+      uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
+      bytes1 b1 = bytes1(temp);
+      bstr[k] = b1;
+      _i /= 10;
+    }
+    return string(bstr);
+  }
+}
