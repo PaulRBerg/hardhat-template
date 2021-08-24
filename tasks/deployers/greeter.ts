eed9f1c23f6f7eed3236f868dcdae83a94c3e886
ahmedali <imahmedalibhatti@gmail.com>
2021-08-24 18:41:57 +0500

    refactor: use verify:verify subtask for programmatic verification

--- tasks/deployers/greeter.ts
+++ tasks/deployers/greeter.ts
@@ -3,20 +3,18 @@ import { TaskArguments } from "hardhat/types";
 
 import { Greeter, Greeter__factory } from "../../typechain";
 
-import { execSync } from "child_process";
-
 task("deploy:Greeter")
   .addParam("greeting", "Say hello, be nice")
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const { name } = await ethers.provider.getNetwork();
+  .setAction(async function (taskArguments: TaskArguments, { ethers, network, run }) {
     const greeterFactory: Greeter__factory = await ethers.getContractFactory("Greeter");
     const greeter: Greeter = <Greeter>await greeterFactory.deploy(taskArguments.greeting);
     await greeter.deployed();
     console.log("Greeter deployed to: ", greeter.address);
 
-    if (name !== "unknown") {
-      execSync(`npx hardhat verify --network ${name} ${greeter.address} ${taskArguments.greeting}`, {
-        stdio: "inherit",
+    if (network.name !== "hardhat") {
+      await run("verify:verify", {
+        address: greeter.address,
+        constructorArguments: [taskArguments.greeting],
       });
     }
   });
