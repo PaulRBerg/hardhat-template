24ffb3b69c6e4a480bd1de5a4a8ebec44f5a7c34
ahmedali <imahmedalibhatti@gmail.com>
2021-08-24 18:50:28 +0500

    refactor: skip verify:verify subtask if apiKey not set

--- tasks/deployers/greeter.ts
+++ tasks/deployers/greeter.ts
@@ -5,13 +5,13 @@ import { Greeter, Greeter__factory } from "../../typechain";
 
 task("deploy:Greeter")
   .addParam("greeting", "Say hello, be nice")
-  .setAction(async function (taskArguments: TaskArguments, { ethers, network, run }) {
+  .setAction(async function (taskArguments: TaskArguments, { config, ethers, network, run }) {
     const greeterFactory: Greeter__factory = await ethers.getContractFactory("Greeter");
     const greeter: Greeter = <Greeter>await greeterFactory.deploy(taskArguments.greeting);
     await greeter.deployed();
     console.log("Greeter deployed to: ", greeter.address);
 
-    if (network.name !== "hardhat") {
+    if (network.name !== "hardhat" && config.etherscan.apiKey) {
       await run("verify:verify", {
         address: greeter.address,
         constructorArguments: [taskArguments.greeting],
