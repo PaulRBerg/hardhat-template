8652e8b2ba2ff34eea2db182e25f3b195e7ba889
Paul Razvan Berg <hello@paulrberg.com>
2022-03-07 14:56:12 +0000

    test: use websocket instead of https for infura URL

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -41,7 +41,7 @@ const chainIds = {
 };
 
 function getChainConfig(network: keyof typeof chainIds): NetworkUserConfig {
-  const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
+  const url: string = "wss://" + network + ".infura.io/ws/v3/" + infuraApiKey;
   return {
     accounts: {
       count: 10,
