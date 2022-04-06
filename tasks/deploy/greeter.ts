c45b1600eed1d5f5f72074d3050a165af6587a3d
Paul Razvan Berg <hello@paulrberg.com>
2022-04-06 14:29:57 +0300

    refactor: set signer deployer in deploy task

--- tasks/deploy/greeter.ts
+++ tasks/deploy/greeter.ts
@@ -1,3 +1,4 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 
@@ -7,8 +8,9 @@ import { Greeter__factory } from "../../src/types/factories/Greeter__factory";
 task("deploy:Greeter")
   .addParam("greeting", "Say hello, be nice")
   .setAction(async function (taskArguments: TaskArguments, { ethers }) {
+    const signers: SignerWithAddress[] = await ethers.getSigners();
     const greeterFactory: Greeter__factory = <Greeter__factory>await ethers.getContractFactory("Greeter");
-    const greeter: Greeter = <Greeter>await greeterFactory.deploy(taskArguments.greeting);
+    const greeter: Greeter = <Greeter>await greeterFactory.deploy(taskArguments.greeting, { from: signers[0].address });
     await greeter.deployed();
     console.log("Greeter deployed to: ", greeter.address);
   });
