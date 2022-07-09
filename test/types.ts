52aa58a6be9bdee8def3f983759cf904cead6b4c
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-07-09 14:22:15 +0500

    refactor: migrate to hardhat chai matchers with loadFixture from hardhat helpers

--- test/types.ts
+++ test/types.ts
@@ -1,7 +1,8 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import type { Fixture } from "ethereum-waffle";
 
 import type { Greeter } from "../src/types/Greeter";
+
+type Fixture<T> = () => Promise<T>;
 
 declare module "mocha" {
   export interface Context {
