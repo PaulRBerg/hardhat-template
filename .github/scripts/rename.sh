a5214e4fccffe0f55ab34a1e16ed9df00cec92cf
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-08-30 22:35:34 +0500

    chore: add rename bash script

---
+++ .github/scripts/rename.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+REPOSITORY=${1?Error: Pass username/repo e.g. @prb/hardhat-template}
+REPOSITORY_OWNER=${2?Error: Pass username e.g. prb}
+
+echo "REPOSITORY: $REPOSITORY"
+echo "REPOSITORY_OWNER: $REPOSITORY_OWNER"
+
+# jq is like sed for JSON data
+JQ_OUTPUT=`jq \
+  --arg NAME "$REPOSITORY" \
+  --arg AUTHOR_NAME "$REPOSITORY_OWNER" \
+  --arg URL "https://github.com/$REPOSITORY_OWNER" \
+  '.name = $NAME | .description = "" | .author |= ( .name = $AUTHOR_NAME | .url = $URL )' \
+  package.json`
+
+# overwrite package.json
+echo "$JQ_OUTPUT" > package.json
