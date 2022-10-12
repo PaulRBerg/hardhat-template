2d823d90e0bf7460f909f231ea24604662afe71f
Paul Razvan Berg <hello@paulrberg.com>
2022-10-12 14:54:36 +0300

    refactor: upgrade to solidity v0.8.17

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -99,7 +99,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.15",
+    version: "0.8.17",
     settings: {
       metadata: {
         // Not including the metadata hash
