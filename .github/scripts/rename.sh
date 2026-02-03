835407733cd6ea0c26798b466bdd381f5f8ef4e1
Zentex <adeeb69@aol.com>
2026-02-03 22:27:31 +0530

    Fix rename script to reference hardhat-template instead of foundry-template

--- .github/scripts/rename.sh
+++ .github/scripts/rename.sh
@@ -31,8 +31,8 @@ sedi () {
   sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
 }
 
-# Rename instances of "PaulRBerg/foundry-template" to the new repo name in README.md for badges only
-sedi "/gitpod/ s|PaulRBerg/foundry-template|"${GITHUB_REPOSITORY}"|;" "README.md"
-sedi "/gitpod-badge/ s|PaulRBerg/foundry-template|"${GITHUB_REPOSITORY}"|;" "README.md"
-sedi "/gha/ s|PaulRBerg/foundry-template|"${GITHUB_REPOSITORY}"|;" "README.md"
-sedi "/gha-badge/ s|PaulRBerg/foundry-template|"${GITHUB_REPOSITORY}"|;" "README.md"
+# Rename instances of "paulrberg/hardhat-template" to the new repo name in README.md for badges only
+sedi "/gitpod/ s|paulrberg/hardhat-template|"${GITHUB_REPOSITORY}"|;" "README.md"
+sedi "/gitpod-badge/ s|paulrberg/hardhat-template|"${GITHUB_REPOSITORY}"|;" "README.md"
+sedi "/gha/ s|paulrberg/hardhat-template|"${GITHUB_REPOSITORY}"|;" "README.md"
+sedi "/gha-badge/ s|paulrberg/hardhat-template|"${GITHUB_REPOSITORY}"|;" "README.md"
